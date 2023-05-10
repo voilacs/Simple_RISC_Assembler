21678112349970cdec051c10f527f6936ad95e78
gargdhawal05 <133147851+gargdhawal05@users.noreply.github.com>
2023-05-10 22:57:57 +0530

    Update co_groupproject1.py

--- co_groupproject1.py
+++ co_groupproject1.py
@@ -1,1 +1,14 @@
-
+def binaryconverter(number):
+    l=[]
+    while number!=0:
+        rem=str(number%2)
+        l.append(rem)
+        number=number//2
+    l.reverse()        
+    while len(l) < 8:
+        l.insert(0,'0')
+    binary_string = ''.join(l)
+    return binary_string
+  
+  
+       
