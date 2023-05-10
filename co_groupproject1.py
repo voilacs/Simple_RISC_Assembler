82438c735d3d37242b9cba4788979d5080af1198
voilacs <111688259+voilacs@users.noreply.github.com>
2023-05-10 22:58:58 +0530

    Update co_groupproject1.py
    added reg_address function by anmol

--- co_groupproject1.py
+++ co_groupproject1.py
@@ -9,6 +9,17 @@ def binaryconverter(number):
         l.insert(0,'0')
     binary_string = ''.join(l)
     return binary_string
-  
-  
+def reg_address(register):
+    '''
+    Use A capital letter + a number to refer to a register into the function 
+    '''
+    registers=['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'FLAGS']
+    values=['000', '001', '010', '011', '100', '101', '110', '111']
+    for i in range(8):
+        if(register==registers[i]):
+            return values[i]
+    return -1
+x=reg_address('R5')
+print(x)
+
        
