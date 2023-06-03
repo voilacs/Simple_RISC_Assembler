441aa40c14017a91e00e3ed09bf542d99c3a9a9c
gargdhawal05 <133147851+gargdhawal05@users.noreply.github.com>
2023-06-03 15:22:15 +0530

    Update execution_engine.py

--- SimpleSimulator/execution_engine.py
+++ SimpleSimulator/execution_engine.py
@@ -25,4 +25,15 @@ def checkOverflow(value):
 #                 RF.setOverflowFlag()
 #             else:
 #                 RF.resetFlagRegister()
+def binaryconverter(number):
+    l=[]
+    while number!=0:
+        rem=str(number%2)
+        l.append(rem)
+        number=number//2
+    l.reverse()        
+    while len(l) < 7:
+        l.insert(0,'0')
+    binary_string = ''.join(l)
+    return binary_string
 
